df36ee6c66b7a41bb0ccd6eb45f032e084a6568d
philipz <philipz.abicki@gmail.com>
2023-12-02 18:20:39 +0100

    macd bot bugfix

--- live_macd.py
+++ live_macd.py
@@ -3,19 +3,19 @@ from credentials import binance_API_KEY, binance_SECRET_KEY
 
 SYMBOL = 'BTCFDUSD'
 stop_loss, enter_at, close_at = 0.25, 1.0, 1.0
-fast_ma_type, fast_period, slow_ma_type, slow_period, signal_ma_type, signal_period = 5, 6, 3, 23, 13, 12
+fast_ma_type, slow_ma_type, signal_ma_type, fast_period, slow_period, signal_period = 5, 6, 3, 23, 13, 12
 INTERVAL = '1m'
 # MA requires previous data longer than just calculation period size
 PREV_DATA_MULTIPLAYER = 150
 SETTINGS = {'SL': stop_loss,
             'enter_at': enter_at,
             'close_at': close_at,
-            'fast_ma_type': fast_ma_type,
-            'slow_ma_type': slow_ma_type,
-            'signal_ma_type': signal_ma_type,
             'fast_period': fast_period,
             'slow_period': slow_period,
-            'signal_period': signal_period}
+            'signal_period': signal_period,
+            'fast_ma_type': fast_ma_type,
+            'slow_ma_type': slow_ma_type,
+            'signal_ma_type': signal_ma_type}
 
 if __name__ == '__main__':
     bot = TakerBot(SYMBOL,
